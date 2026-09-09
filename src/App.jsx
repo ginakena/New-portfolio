e5a1a6a683525a9c9eeff16fcafb4b3f8e2e3ce4
ginakena <makenareginam@gmail.com>
2026-09-09 08:10:31 +0300

    style github and linkedin icons

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Sidebar from "./components/sidebar";
 import Hero from "./components/hero";
 import About from "./components/about";
 import Projects from "./components/projects";
-import Experience from "./components/Experience";
+import Experience from "./components/experience";
 import Contact from "./components/contact";
 
 export default function App() {
